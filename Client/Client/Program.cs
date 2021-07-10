db7f567920639cc8afec1c84e74f9ea50e2866e7
Khonsa Nadziroh <63987464+knadziroh@users.noreply.github.com>
2021-07-10 23:07:11 +0700

    Add files via upload

---
+++ Client/Client/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+public class Client
+{
+    private static NetworkStream stream;
+    public static TcpClient socketForServer;
+    
+    //fungsi utama(main)
+    static public void Main(string[] args)
+    {
+        
+        try
+        {
+            socketForServer = new TcpClient("127.0.0.1", 5000);
+        }
+        catch
+        {
+            Console.WriteLine("Failed to connect to server at {0}:999", "localhost");
+            return;
+        }
+
+        NetworkStream networkStream = socketForServer.GetStream();
+        
+        System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(socketForServer.GetStream());
+        Console.WriteLine("******* Client Program that Connected to Localhost on port No:10 *****");
+
+        try
+        {
+            Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+            receiveThread.Start();
+            Console.WriteLine("Type your message : ");
+            string str = Console.ReadLine();
+            while (str != "EXIT")
+            {
+                streamWriter.WriteLine(str);
+                streamWriter.Flush();
+                Console.WriteLine("Type your message : ");
+                str = Console.ReadLine();
+            }
+            if (str == "EXIT")
+            {
+                streamWriter.WriteLine(str);
+                streamWriter.Flush();
+            }
+        }
+
+        catch
+        {
+            Console.WriteLine("Exception reading from Server");
+        }
+        networkStream.Close();
+        Console.WriteLine("Press any key to exit from client program . . .");
+        Console.ReadKey();
+    }
+
+    //fungsi menerima chat broadcast
+    static void ReceiveMessage()
+    {
+        System.IO.StreamReader streamReader = new System.IO.StreamReader(socketForServer.GetStream());
+        while (true)
+        {
+            try
+            {
+                byte[] data = new byte[64];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+                string msg = streamReader.ReadLine();
+                Console.WriteLine(msg);
+            }
+
+            catch(Exception e)
+            {
+                Console.Write(e.Message);
+                break;
+            }
+        }
+    }
+
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+public class Server
+{
+    public static TcpListener tcpListener;
+    public static List<TcpClient> tcpClientLists = new List<TcpClient>();
+
+    //membaca input dari client
+    static void Listeners()
+    {
+        TcpClient tcpClient = tcpListener.AcceptTcpClient();
+        tcpClientLists.Add(tcpClient);
+            System.IO.StreamReader streamReader = new System.IO.StreamReader(tcpClient.GetStream());
+            while (true)
+            {
+                string theString = streamReader.ReadLine();
+                Console.WriteLine("Message recieved by client :" + theString);
+                Broadcast(theString, tcpClient);
+                if (theString == "exit")
+                    break;
+            }
+            streamReader.Close();
+
+        Console.WriteLine("Press any key to exit from server program . . .");
+        Console.ReadKey();
+    }
+
+    //fungsi broadcast untuk menyebarkan chat ke client lain
+    public static void Broadcast(string msg, TcpClient tcpClient)
+    {
+        foreach(TcpClient client in tcpClientLists)
+        {
+            System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(client.GetStream());
+            if(client != tcpClient)
+            {
+              streamWriter.WriteLine("Client : " + msg);
+            }
+            streamWriter.Flush();
+        }
+    }
+
+    //fungsi utama(main)
+    public static void Main()
+    {
+        tcpListener = new TcpListener(IPAddress.Any, 5000);
+        tcpListener.Start();
+        
+        Console.WriteLine("************ Server Program ************");
+        while (true)
+        {
+            Thread newThread = new Thread(new ThreadStart(Listeners));
+            newThread.Start();
+        }
+    }
+}
